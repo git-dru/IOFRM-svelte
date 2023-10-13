4484e0b5f0493c60c6b81cc4b49202bf72e736a6
git-dru <mastercrypto.us@gmail.com>
2023-10-13 10:34:32 +0300

    build dom with svelte/tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,122 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let isLogin = true;
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="IOFRM App" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-	<h3 class="text-red-600 underline">text</h3>
-
-	<Counter />
+<section class="flex relative w-full h-screen bg-[#23394a] text-white">
+	<div class="website-logo fixed top-1/2 w-[100px] left-[220px]">
+		<div class="bg-logo-dark">
+			<img class="opacity-0 cursor-pointer" src="/images/logo-light.svg" alt="logo image" />
+		</div>
+	</div>
+	<div class="bg-back-image w-[550px] bg-center bg-cover" />
+	<div class="bg-custom-background max-w-[340px] mx-auto my-auto text-left form-content">
+		<h3 class="text-white text-[24px] font-bold mb-4">Get more things done with Loggin platform</h3>
+		<p class="text-white text-[18px] font-thin mb-8">Access to the most powerful tool in the entire design and web industry</p>
+		<div class="cursor-pointer flex mt-3 mb-[34px] page-links">
+			<div class="relative text-[15px] mr-[20px] bg-transparent {isLogin ? 'text-bold' : 'text-medium'}" on:click={() => {isLogin = true}}>Login</div>
+			<div class="relative text-[15px] mr-[20px] bg-transparent {!isLogin ? 'text-bold' : 'text-medium'}" on:click={() => {isLogin = false}}>Register</div>
+		</div>
+		<form class="form-input">
+			<div class="data-input">
+				{#if !isLogin}
+				<input
+					class="border border-gray-300 rounded-md px-3 py-2 mb-4"
+					placeholder="Full Name"
+					required
+				/>
+				{/if}
+				<input
+					class="border border-gray-300 rounded-md px-3 py-2 mb-4"
+					placeholder="E-mail Address"
+					required
+				/>
+				<input
+					type="password"
+					class="border border-gray-300 rounded-md px-3 py-2"
+					placeholder="Password"
+					required
+				/>
+			</div>
+			{#if isLogin}
+			<div class="flex items-center mt-3">
+				<input
+					id="default-checkbox"
+					type="checkbox"
+					value=""
+					class="w-4 h-4 text-custom-background bg-gray-100 rounded-md focus:ring-0 border border-white dark:border-gray-800 dark:bg-transparent dark:checked:bg-white"
+				/>
+				<label
+					for="default-checkbox"
+					class="ml-2 text-sm font-semibold text-white-700 dark:text-gray-300">Remember me</label
+				>
+			</div>
+			{/if}
+			<div class="form-button mt-[30px] mb-[30px]">
+				{#if isLogin}
+				<button
+					type="submit"
+					class="bg-white font-medium rounded-md text-xs w-full sm:w-auto px-6 py-2 text-center"
+					>Login</button
+				>
+				<a class="text-white text-xs ml-3" href="/forget">Forget password?</a>
+				{:else}
+				<button
+					type="submit"
+					class="bg-white font-medium rounded-md text-xs w-full sm:w-auto px-6 py-2 text-center"
+					>Register</button
+				>
+				{/if}
+			</div>
+		</form>
+		<div class="other-links text-[12px] mt-3">
+			{#if isLogin}
+			<span>Or login with</span>
+			{:else}
+			<span>Or register with</span>
+			{/if}
+			<a class="ml-3 text-white font-medium" href="#">Facebook</a>
+			<a class="ml-3 text-white font-medium" href="#">Google</a>
+			<a class="ml-3 text-white font-medium" href="#">Linkedin</a>
+		</div>
+	</div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
+	.form-content .page-links div:after {
 		position: absolute;
+		background-color: white;
+		content: "";
 		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		height: 2px;
+		left: 0;
+		bottom: -10px;
+		transition: all 0.3s;
+	}
+
+	.form-content .data-input input {
+		font-size: 15px;
+		padding: 9px 20px;
+		outline: 0;
+		border: 0;
+		background-color: #445867;
+		width: 100%;
+		height: 36px;
+	}
+
+	.form-content .data-input input:hover,
+	.form-content .data-input input:focus {
+		background-color: #fff;
+		color: #23394a;
+	}
+
+	.form-content .form-button button {
+		color: #23394a;
+		font-weight: 700;
 	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'logo-dark': "url('/images/logo-dark.svg')",
+        'logo-light': "url('/images/logo-light.svg')",
+        'back-image': "url('/images/background.jpg')",
+      },
+      colors: {
+        'custom-background': "#23394a",
+      }
+    },
   },
   plugins: [],
 }
